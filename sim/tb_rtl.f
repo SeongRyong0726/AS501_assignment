95ca706af8de8b0b9da081afa7019c0b6e47f0b1
jiwonchoi99 <jw.choi@kaist.ac.kr>
2024-05-29 00:00:09 +0900

    sim env

---
+++ sim/tb_rtl.f
@@ -0,0 +1,32 @@
+#==== Testbench & Tester ====#
+./tb/core_tb_10.sv
+
+
+#==== DUT Top ====#
+../rtl/cpu_top.sv"
+
+#==== Common ====#
+../rtl/common/core_package.sv                  
+../rtl/common/d_flip_flop.sv                   
+../rtl/common/mux2to1.sv                       
+../rtl/common/mux3to1.sv                       
+../rtl/common/mux4to1.sv                       
+../rtl/common/mux5to1.sv                       
+../rtl/common/counter.sv                       
+
+#==== Memory ====#
+../rtl/memory/memory_top.sv                    
+/technology/SAED32/lib/sram/verilog/saed32sram.v 
+../rtl/cache/submodule/cache_sram.sv           
+../rtl/cache/instr_cache.sv                    
+
+#==== Scalar Core ====#
+../rtl/scalar_core/alu.sv                      
+../rtl/scalar_core/csr.sv                      
+../rtl/scalar_core/decoder.sv                  
+../rtl/scalar_core/regfile.sv                  
+../rtl/scalar_core/scalar_core.sv
+
+
+
+              
